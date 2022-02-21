8283eb0009e6fe038b9354cfe8641191143e6a74
Kostiskat <80099327+Kostiskat@users.noreply.github.com>
2022-02-21 20:26:14 +0200

    work

--- src/commands/Information/suggest.js
+++ src/commands/Information/suggest.js
@@ -13,7 +13,7 @@ module.exports = {
         .setTitle(`New suggestion from ${interaction.user.username}`)
         .setColor('#6afc91')
         .setDescription(`New suggestion from <@${interaction.user.id}>. Please vote using the appropriate emojis for it to be more easily recognized by the staff team. \n\n > *${interaction.options.getString('suggestion')}*`)
-        await client.guilds.cache.get('919708634596913172').channels.cache.get("921817257703780434").send({embeds: [suggEmbed]}).then(async message => {
+        await interaction.guild.channels.cache.get("921817257703780434").send({embeds: [suggEmbed]}).then(async message => {
            await message.react("<:like:927016619010580511> ")
            await message.react("<:dislike:927016619241267260>")
         })
