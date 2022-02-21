febd65e494bdf032a0068f8ad67ed7c7e3be729b
Kostiskat <80099327+Kostiskat@users.noreply.github.com>
2022-02-21 20:16:10 +0200

    suggestion shit

---
+++ src/commands/Information/suggest.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const datahandler = require("../../dataHandler")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('suggest')
+		.setDescription("Issue a suggestion for Bristo.")
+        .addStringOption(option => option.setName('suggestion').setRequired(true).setDescription("Your suggestion")),
+	async execute(interaction, client) {
+        await datahandler.checkUserProfile(interaction.user.id)
+		const suggEmbed = new MessageEmbed()
+        .setTitle(`New suggestion from ${interaction.user.username}`)
+        .setColor('#6afc91')
+        .setDescription(`New suggestion from <@${interaction.user.id}>. Please vote using the appropriate emojis for it to be more easily recognized by the staff team. \n\n > *${interaction.options.getString('suggestion')}*`)
+        await client.guilds.cache.get('919708634596913172').channels.cache.get("921817257703780434").send({embeds: [suggEmbed]}).then(async message => {
+           await message.react("<:like:927016619010580511> ")
+           await message.react("<:dislike:927016619241267260>")
+        })
+	},
+};
